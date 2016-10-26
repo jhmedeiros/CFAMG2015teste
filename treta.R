b74483f11e4264b17019dacc874528dd1e2a2185
jhmedeiros <jhmedeiros@hotmail.de>
2016-10-26 11:26:12 -0200

    Update treta.R

--- treta.R
+++ treta.R
@@ -9,11 +9,26 @@ setwd("D:/Trabalho/TCE/Teste API/TesteAPI/data")
 a <- read.csv("planilha_2015.csv", sep = ";")
 str(a)
 
-TESTE_filtro <- function(a, area_de_resultado, tipo_de_programa) {
+TESTE_filtro <- function(a, area_de_resultado, tipo_de_programa, dimensao_da_subacao) {
     
-    resultado <- a %>% filter(area_resultado %in% area_de_resultado,
-                              tipo_programa %in% tipo_de_programa)
-    
+        if(dimensao_da_subacao == "indicadores_agrupamento_Fisica"){
+        
+                resultado <- a %>% filter(area_resultado %in% area_de_resultado,
+                                          tipo_programa %in% tipo_de_programa)
+                
+                resultado <- resultado[ , c(1,2,3)]
+                
+        }
+        
+        else if(dimensao_da_subacao == "indicadores_agrupamento_Financeira"){
+                
+                resultado <- a %>% filter(area_resultado %in% area_de_resultado,
+                                          tipo_programa %in% tipo_de_programa)
+                
+                resultado <- resultado[ , c(1,2,4)]
+                
+        }
+        
     print(resultado)
 
 }
